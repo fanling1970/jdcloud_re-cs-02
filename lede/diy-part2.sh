9f0084b5a8bfa1d702b34fd5bb4183eec2b4e4d9
fanling1970 <zghqidai2008@sina.com>
2026-06-18 00:04:12 +0800

    Update diy-part2.sh

--- lede/diy-part2.sh
+++ lede/diy-part2.sh
@@ -139,7 +139,7 @@ echo "配置系统基础设置..."
 
 cat > files/etc/config/system << 'EOF'
 config system
-    option hostname 'LEDE'
+    option hostname 'JDCloud-AX6600'
     option timezone 'CST-8'
     option ttylogin '0'
     option log_size '64'
